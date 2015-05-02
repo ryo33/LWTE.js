0fd6da3faa93dccb22642ff283a49b79587b39b6
ryo33 <ryo12redstone@gmail.com>
2015-05-03 02:29:59 +0900

    :sleeping:

--- lwjste.js
+++ lwjste.js
@@ -6,11 +6,18 @@ LWJSTE.prototype = {
     _parseTemplate : function(str){
         var LITERAL = 0, EACH = 1, IF = 2, ELIF = 3, ELSE = 4, ID = 5, SWITCH = 6, CASE = 7, LITERAL = 8, L_CB = 9, R_CB = 10, OPERATOR = 11, EACH_END = 12, IF_END = 13, SWITCH_END = 14, DEFAULT = 15;
         var tokens = [];
+        //lexer
         var token = "";
         var literal = true;
         var last_is_escape = false;
         var operators = ["(", ")", "&", "|"];
         var pushed = false;
+        function success(result){
+            return [0, result];
+        }
+        function faild(result){
+            return [1, result];
+        }
         function push(type, lexeme){
             if(lexeme.length != 0){
                 tokens.push([type, lexeme]);
@@ -77,8 +84,155 @@ LWJSTE.prototype = {
         }else{
             //TODO error
         }
-        return tokens;
-        //TODO
+        //syntactic analysis
+        var progress = 0;
+        var token_length = tokens.length;
+        var result = []; //output of parser
+        var node = result; //pointer to result
+        var indexes = []; //indexes of result
+        var parent_nodes = []; //used in move_node_parent
+        var cb_open = false; //it's true when in curly brackets 
+        var in_cb = []; //tokens in curly brackets
+        var if_opens = 0, else_used = []; //used when in if blocks
+        var each_opens = 0; //used when in each blocks
+        var switch_opens = 0, default_used = []; //used when in switch blocks
+        function drop(array, index){
+            array.splice(index, 1);
+        }
+        function move_node_parent(count){
+            drop(indexes, indexes.length - 1);
+            indexes[indexes.length - 1] = parent_nodes[parent_nodes.length - 1];
+            drop(parent_nodes, parent_nodes.length - 1);
+            if(count != undefined && count > 1){
+                move_node_parent(count - 1);
+            }
+        }
+        function move_node_child(depth, index){
+            parent_nodes.push(indexes[indexes.length - 1]);
+            indexes.push(index);
+        }
+        function move_node(index){
+            indexes[indexes.length - 1] = index;
+        }
+        function get_current_node(){
+            node = result;
+            for(var i = 0, len = indexes.length; i < len; i ++){
+                node = node[indexes[i]];
+            }
+        }
+        while(progress < token_length){
+            get_current_node();
+            if(! cb_open){
+                switch(tokens[progress][0]){
+                    case LITERAL:
+                        node.push(tokens[progress]);
+                        break;
+                    case L_CB:
+                        cb_open = true;
+                        in_cb = [];
+                        break;
+                    default:
+                        //TODO error
+                }
+            }else{
+                if(tokens[progress][0] == R_CB){
+                    cb_open = false;
+                    switch(in_cb[0][0]){
+                        case IF:
+                            if(in_cb.length == 1){
+                                //TODO error
+                            }else{
+                                else_used.push(false);
+                                drop(in_cb, 0);
+                                node.push([IF, [[parse_expression(in_cb), []]], []]);
+                                move_node_child(node.length - 1);
+                                move_node_child(1);
+                                move_node_child(0);
+                                move_node_child(1);
+                            }
+                            break;
+                        case ELIF:
+                            if(if_opens == 0 || in_cb.length == 1 || else_used[if_opens]){
+                                //TODO error
+                            }else{
+                                drop(in_cb, 0);
+                                move_node_parent(2);
+                                node = get_current_node();
+                                node.push([parse_expression(in_cb), []]);
+                                move_node_child(node.length - 1);
+                                move_node_child(1);
+                            }
+                            break;
+                        case ELSE:
+                            if(if_opens == 0 || else_used[if_opens] || in_cb.length != 1){
+                                //TODO error
+                            }else{
+                                else_used[if_opens] = true;
+                                move_node_parent(3);
+                                node = get_current_node();
+                                node.push([]);
+                                move_node_child(0);
+                            }
+                            break;
+                        case IF_END:
+                            if(if_opens == 0 || in_cb.length != 1){
+                                //TODO error
+                            }else{
+                                if(else_used[if_opens]){
+                                    drop(else_used, if_opens);
+                                    move_node_parent(2);
+                                }else{
+                                    move_node_parent(3);
+                                }
+                                if_opens --;
+                            }
+                            break;
+                        case EACH:
+                            if(in_cb.length != 2){
+                                //TODO error
+                            }
+                            break;
+                        case EACH_END:
+                            if(each_opens == 0 || in_cb.length != 1){
+                                //TODO error
+                            }else{
+                            }
+                            break;
+                        case SWITCH:
+                            if(in_cb.length != 2){
+                                //TODO error
+                            }
+                            break;
+                        case CASE:
+                            if(switch_opens == 0 || default_used[switch_opens] || in_cb.length == 1){
+                                //TODO error
+                            }else{
+                            }
+                            break;
+                        case DEFAULT:
+                            if(switch_opens == 0 || default_used[switch_opens] || in_cb.length != 1){
+                                //TODO error
+                            }else{
+                            }
+                            break;
+                        case SWITCH_END:
+                            if(switch_opens == 0 || in_cb.length != 1){
+                                //TODO error
+                            }else{
+                            }
+                            break;
+                        case ID:
+                            break;
+                        default:
+                            //TODO error
+                    }
+                    in_cb = [];
+                }
+                in_cb.push(tokens[progress]);
+            }
+            progress ++;
+        }
+        return success(result);
     },
     addTemplate : function(name, template){
         this.templates[name] = this._parseTemplate(template);
