4271252a388da6fa3514f36f953dd02092ad5992
ryo33 <ryo12redstone@gmail.com>
2015-05-02 22:41:42 +0900

    implement the lexer part of parse

--- lwjste.js
+++ lwjste.js
@@ -3,18 +3,21 @@ function LWJSTE(){
     arguments.callee.escape_character = "#";
 }
 LWJSTE.prototype = {
-    _parseTemplate : function(template){
-        var LITERAL = 0, EACH = 1, IF = 2, ELIF = 3, ELSE = 4, ID = 5, SWITCH = 6, CASE = 7, LITERAL = 8, L_CB = 9, R_CB = 10, OPERATOR = 11;
+    _parseTemplate : function(str){
+        var LITERAL = 0, EACH = 1, IF = 2, ELIF = 3, ELSE = 4, ID = 5, SWITCH = 6, CASE = 7, LITERAL = 8, L_CB = 9, R_CB = 10, OPERATOR = 11, EACH_END = 12, IF_END = 13, SWITCH_END = 14;
         var tokens = [];
         var token = "";
         var literal = true;
         var last_is_escape = false;
         var operators = ["(", ")", "&", "|"];
-        function push(type, lexeme=null){
-            tokens.push([type, lexeme]);
-            token = "";
+        var pushed = false;
+        function push(type, lexeme){
+            if(lexeme.length != 0){
+                tokens.push([type, lexeme]);
+                token = "";
+            }
         }
-        for(var i = 0, len = template.length; i < len; i ++){
+        for(var i = 0, len = str.length; i < len; i ++){
             if(literal){
                 if(last_is_escape){
                     last_is_escape = false;
@@ -28,8 +31,7 @@ LWJSTE.prototype = {
                         last_is_escape = true;
                     }else if(str[i] == "{"){
                         push(LITERAL, token);
-                        push(L_CB);
-                        token = "";
+                        push(L_CB, "{");
                         literal = false;
                     }else{
                         token += str[i];
@@ -37,37 +39,52 @@ LWJSTE.prototype = {
                 }
             }else{
                 if(str[i] == "}"){
-                    items = token.split(" ");
-                    if(items.length == 0){
-                    }else if(items.length == 1){
-                        push(ID, token);
-                    }else{
-                        switch(token.toLowerCase()){
-                            case "if":
-                                break;
-                            case "elif":
-                                break;
-                            case "else":
-                                break;
-                            case "each":
-                                break;
-                            case "switch":
-                                break;
-                            case "case":
-                                break;
+                    if(token.length != 0){
+                        if(pushed){
+                            push(LITERAL, token);
+                        }else{
+                            push(ID, token);
                         }
                     }
+                    push(R_CB, "}");
+                    pushed = false;
                     literal = true;
-                    push(R_CB);
-                }else if(str[i] == " "){
+                }else if(str[i] == " " || operators.indexOf(str[i]) != -1){
+                    if(token.length != 0){
+                        if(pushed){
+                            push(ID, token);
+                        }else{
+                            pushed = true;
+                            switch(token.toLowerCase()){
+                                case "if": push(IF, token); break;
+                                case "elif": push(ELIF, token); break;
+                                case "else": push(ELSE, token); break;
+                                case "/if": push(IF_END, token); break;
+                                case "each": push(EACH, token); break;
+                                case "/each": push(EACH_END, token); break;
+                                case "switch": push(SWITCH, token); break;
+                                case "case": push(CASE, token); break;
+                                case "/switch": push(SWITCH_END, token); break;
+                                default: pushed = false;
+                                    //TODO error
+                            }
+                        }
+                    }
+                    if(str[i] != " "){
+                        push(OPERATOR, str[i]);
+                    }
+                }else{
                     token += str[i];
                 }
             }
         }
-        if(literal){
+        if(literal && token.length != 0){
             push(LITERAL, token);
-            token = "";
+        }else{
+            //TODO error
         }
+        return tokens;
+        //TODO
     },
     addTemplate : function(name, template){
         this.templates[name] = this._parseTemplate(template);
