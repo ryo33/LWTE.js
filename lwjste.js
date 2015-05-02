736849f3ebfb0d10d41b513d06eea9c54b6d4e6f
ryo33 <ryo12redstone@gmail.com>
2015-05-02 18:10:59 +0900

    implement the literal part of parser

--- lwjste.js
+++ lwjste.js
@@ -1,39 +1,47 @@
 function LWJSTE(){
     this.templates = {};
+    arguments.callee.escape_character = "#";
 }
 LWJSTE.prototype = {
     _parseTemplate : function(template){
-        var LITERAL = 0, EACH = 1, IF = 2, ELIF = 3, ELSE = 4, ID = 5, SWITCH = 6, CASE = 7, LITERAL = 8, OPEN = 9, CLOSE = 10;
+        var LITERAL = 0, EACH = 1, IF = 2, ELIF = 3, ELSE = 4, ID = 5, SWITCH = 6, CASE = 7, LITERAL = 8, L_CB = 9, R_CB = 10, OPERATOR = 11;
         var tokens = [];
         var token = "";
         var literal = true;
-        var last_is_open = false;
+        var last_is_escape = false;
+        var operators = ["(", ")", "&", "|"];
         function push(type, lexeme=null){
             tokens.push([type, lexeme]);
             token = "";
         }
         for(var i = 0, len = template.length; i < len; i ++){
             if(literal){
-                if(last_is_open){
-                    last_is_open = false;
-                    if(str[i] == "{"){
-                        token += "{";
+                if(last_is_escape){
+                    last_is_escape = false;
+                    if(str[i] == LWJSTE.escape_character){
+                        token += LWJSTE.escape_character;
                     }else{
-                        push(LITERAL, token);
-                        push(OPEN);
-                        token = "";
-                        literal = false;
+                        token += LWJSTE.escape_character + str[i];
                     }
                 }else{
-                    if(str[i] == "{"){
-                        last_is_open = true;
+                    if(str[i] == LWJSTE.escape_character){
+                        last_is_escape = true;
+                    }else if(str[i] == "{"){
+                        push(LITERAL, token);
+                        push(L_CB);
+                        token = "";
+                        literal = false;
                     }else{
                         token += str[i];
                     }
                 }
             }else{
                 if(str[i] == "}"){
-                    if(token.length != 0){
+                    items = token.split(" ");
+                    if(items.length == 0){
+                    }else if(items.length == 1){
+                        push(ID, token);
+                    }else{
                         switch(token.toLowerCase()){
                             case "if":
                                 break;
@@ -48,18 +56,12 @@ LWJSTE.prototype = {
                             case "case":
                                 break;
                         }
-                        push(ID, token);
                     }
                     literal = true;
-                    push(CLOSE);
+                    push(R_CB);
                 }else if(str[i] == " "){
-                    if(token.length != 0){
-                        push(ID, token);
-                    }
-                }else{
                     token += str[i];
                 }
-                //TODO
             }
         }
         if(literal){
